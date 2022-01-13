f6a5559734b2776319684d01aeae1ad6d9c4e0b8
Sylwia Świat <sylwiaswiat2@wp.pl>
2022-01-13 13:08:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+ window.addEventListener('load', ()=>{
+     const preloader = document.querySelector('.loader-wrapper');
+     preloader.classList.add('loaded')
+ })
+
+ let slideImages = document.querySelectorAll('.slide');
+ let arrowRight = document.querySelector('#arrowRight');
+ let arrowLeft = document.querySelector('#arrowLeft');
+ let current = 0;
+
+ const reset = ()=>{
+for(let i=0; i<slideImages.length; i++){
+    slideImages[i].style.display='none';
+}
+ }
+
+ const startSlide = ()=>{
+     reset();
+     slideImages[0].style.display = 'block';
+ }
+
+ const showPrev = ()=>{
+     reset();
+     slideImages[current -1].style.display = 'block';
+     current--;
+ }
+
+ arrowLeft.addEventListener('click', ()=>{
+     if(current===0){
+         current = slideImages.length;
+     }
+     showPrev();
+ })
+
+const showNext = ()=>{
+    reset();
+    slideImages[current + 1].style.display = 'block';
+    current++;
+}
+
+arrowRight.addEventListener('click', ()=>{
+if(current === slideImages.length - 1){
+current=-1
+}
+showNext();
+})
+
+ startSlide();
