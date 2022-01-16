eee5d37b08ab17641017ecd9b088320741b09539
V17_ZEN\Hyperbook <dariusz.slabenski@gmail.com>
2022-01-16 13:30:33 +0100

    refactor kodu.

--- scripts/script.js
+++ scripts/script.js
@@ -1,63 +1,62 @@
- window.addEventListener('load', ()=>{
-     const preloader = document.querySelector('.loader-wrapper');
-     preloader.classList.add('loaded')
- })
+window.addEventListener('load', () => {
+    const preloader = document.querySelector('.loader-wrapper');
+    preloader.classList.add('loaded')
+})
 
- AOS.init();
+AOS.init();
 
- let slideImages = document.querySelectorAll('.slide');
- let arrowRight = document.querySelector('#arrowRight');
- let arrowLeft = document.querySelector('#arrowLeft');
- let current = 0;
- let galleryPhotos = document.querySelectorAll('.galleryPhoto');
+let slideImages = document.querySelectorAll('.slide');
+let arrowRight = document.querySelector('#arrowRight');
+let arrowLeft = document.querySelector('#arrowLeft');
+let current = 0;
+let galleryPhotos = document.querySelectorAll('.galleryPhoto');
 
- const reset = ()=>{
-for(let i=0; i<slideImages.length; i++){
-    slideImages[i].style.display='none';
+const reset = () => {
+    for (let i = 0; i < slideImages.length; i++) {
+        slideImages[i].style.display = 'none';
+    }
 }
- }
 
- const startSlide = ()=>{
-     reset();
-     slideImages[0].style.display = 'block';
- }
+const startSlide = () => {
+    reset();
+    slideImages[0].style.display = 'block';
+}
 
- const showPrev = ()=>{
-     reset();
-     slideImages[current -1].style.display = 'block';
-     current--;
+const showPrev = () => {
+    reset();
+    slideImages[current - 1].style.display = 'block';
+    current--;
 
- }
+}
 
- arrowLeft.addEventListener('click', ()=>{
-     if(current===0){
-         current = slideImages.length;
-     }
-     showPrev();
- })
+arrowLeft.addEventListener('click', () => {
+    if (current === 0) {
+        current = slideImages.length;
+    }
+    showPrev();
+})
 
-const showNext = ()=>{
+const showNext = () => {
     reset();
     slideImages[current + 1].style.display = 'block';
     current++;
-    
+
 }
 
-arrowRight.addEventListener('click', ()=>{
-if(current === slideImages.length - 1){
-current=-1
-}
-showNext();
+arrowRight.addEventListener('click', () => {
+    if (current === slideImages.length - 1) {
+        current = -1
+    }
+    showNext();
 })
 
- startSlide();
+startSlide();
 
-galleryPhotos.forEach((galleryPhoto)=>{
-    galleryPhoto.addEventListener('click', ()=>{
+galleryPhotos.forEach((galleryPhoto) => {
+    galleryPhoto.addEventListener('click', () => {
         galleryPhoto.classList.toggle('fullScreen')
     })
 })
-     
- 
- 
- 
+
+
+
